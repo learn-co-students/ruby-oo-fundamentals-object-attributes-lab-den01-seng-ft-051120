4549ada57ff9444ca6c5a87aaccc00331edb654b
Kyle Petersen <kpete2017@gmail.com>
2020-05-14 10:39:08 -0600

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,24 @@
+require 'pry'
+# top leve class comment
+class Dog
+  def initialize
+    @name
+    @breed
+  end
+
+  def name=(new_name)
+    @name = new_name
+  end
+
+  def name
+    @name
+  end
+
+  def breed=(new_breed)
+    @breed = new_breed
+  end
+
+  def breed
+    @breed
+  end
+end
